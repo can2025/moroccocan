eff46956cf2df4d368f2d9ad6db822dc4e520eaf
can2025 <bolt.can.2025@gmail.com>
2025-07-15 23:08:06 +0200

    Updated groups.tsx

--- app/(tabs)/groups.tsx
+++ app/(tabs)/groups.tsx
@@ -1,7 +1,6 @@
 import React, { useState } from 'react';
 import { View, Text, ScrollView, StyleSheet, TouchableOpacity } from 'react-native';
 import { SafeAreaView } from 'react-native-safe-area-context';
-import FlagIcon from '@/components/FlagIcon';
 import Flag from 'react-world-flags';
 
 const groupsData = {
