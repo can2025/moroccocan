189092eca8eb58e4bfa0de901003bbbeb334a8fe
can2025 <bolt.can.2025@gmail.com>
2025-07-15 22:22:52 +0200

    Updated index.tsx

--- app/(tabs)/index.tsx
+++ app/(tabs)/index.tsx
@@ -99,7 +99,7 @@ export default function HomeScreen() {
                 <View style={styles.teamContainer}>
                   <Text style={styles.score}>{match.awayScore}</Text>
                   <Text style={styles.teamName}>{match.awayTeam}</Text>
-                  <Flag code='tn' size={32} />
+                  <Flag code='tn' style={{ width: 32, height: 32, margin: '10px' }} />
                 </View>
               </View>
               
